bd64fc082528c8d3aacd1080220f254bc84793ea
Mohamed Fawas <Mohammedfawasfareed@gmail.com>
2025-09-25 22:36:02 +0530

    Update page.tsx

--- components/App/page.tsx
+++ components/App/page.tsx
@@ -1,14 +1,20 @@
 // components/App.tsx
 import React from 'react';
+const reviews = [
+  {
+    name: "ANNE PEREIRA",
+    text: `One of the best spots for affordable and delicious burgers — my all-time favorites are the Thunderbolt and Nitro burgers: nice flavor and totally worth it.`,
+    rating: 5,
+  },
+  {
+    name: "JONOE DOE",
+    text: `The food was delicious... every burger was juicy and full of flavor... nice, cozy atmosphere... parking is spacious.`,
+    rating: 5,
+  },
+  {
+    name: "JACK SKEE",
+    text: `The burgers are juicy, flavorful, and creatively crafted. The "Nitro" burger is fan-favorite — tasty, never exceptionally friendly, helpful, and attentive.`,
+    rating: 5,
+  },
+];
 
-const App: React.FC = () => {
-  return (
-    <section id="app" className="flex items-center justify-center min-h-screen">
-      <h1 className="text-5xl font-bold text-center">
-        App & Customer Service Here
-      </h1>
-    </section>
-  );
-};
-
-export default App;
