6cb8b2b5d527e4ce23921812b91e93eb1c3c1c0e
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-02-02 17:09:40 +0530

    change private access

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -35,8 +35,7 @@ module "eks" {
     }
   }
 
-  endpoint_public_access = true
-  endpoint_private_access = false
+  endpoint_private_access = true
 
   enable_cluster_creator_admin_permissions = true
 
