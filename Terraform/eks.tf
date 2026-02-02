fbea68a9b89af5ad888cd9b7951b11d313cfc5c4
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-02-02 14:17:21 +0530

    add  controll  plane subnet

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -43,6 +43,8 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
+
 
 
   eks_managed_node_groups = {
